fe24981bb5015f1dadaa9a3c9e9de23651bff29f
297621 <71358523+297621@users.noreply.github.com>
2021-05-21 12:35:04 +0800

    Update UserService.java

--- src/main/java/edu/wut/dbexp/Service/UserService.java
+++ src/main/java/edu/wut/dbexp/Service/UserService.java
@@ -5,4 +5,11 @@ package edu.wut.dbexp.Service;
  * @date 2021/5/219:08
  */
 public interface UserService {
+     boolean addUser(User user);
+
+     boolean updateUser(User user);
+
+     boolean existUser(User user);
+
+     boolean deleteUser(User user);
 }
