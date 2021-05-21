5fdb61f6f1f5c65a061c643a8c7ca536fb44307b
297621 <71358523+297621@users.noreply.github.com>
2021-05-21 12:34:26 +0800

    Update GoodsService.java

--- src/main/java/edu/wut/dbexp/Service/GoodsService.java
+++ src/main/java/edu/wut/dbexp/Service/GoodsService.java
@@ -5,4 +5,15 @@ package edu.wut.dbexp.Service;
  * @date 2021/5/219:09
  */
 public interface GoodsService {
+    boolean addGoods(Goods goods);
+
+    boolean updateGoods(Goods goods);
+
+    boolean deleteGoods(Goods goods);
+
+    Goods searchGoods(String id);      //通过货物编号查找相应货物信息并返回
+
+    List<Goods> allGoods();
+
+    List<Goods> selectedGoods(int num); //返回仓库中少于num件的货物信息
 }
